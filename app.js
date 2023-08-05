1e439a60ff0b4a32612a10fa64633412be1ad101
Harada <1a88.19e7@gmail.com>
2023-08-05 13:32:03 +0900

    updates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ new Vue({
   data: {
     number: '',
     number2: '',
+    oyasumi: '',
     showNumber: false,
     showNumber2: false
   },
@@ -12,14 +13,14 @@ new Vue({
       const keyCode = event.keyCode || event.which;
       const firstRespondent = document.getElementsByClassName("first-respondent")[0];
       const secondRespondent = document.getElementsByClassName("second-respondent")[0];
-      if (keyCode >= 48 && keyCode <= 57 && !this.showNumber) {
+      if (keyCode >= 48 && keyCode <= 57 && !this.showNumber && key != this.oyasumi) {
         this.number = key;
         this.showNumber = true;
         playBuzzer();
-      } else if (keyCode >= 48 && keyCode <= 57 && this.showNumber && !this.showNumber2 && key != this.number){
+      } else if (keyCode >= 48 && keyCode <= 57 && this.showNumber && !this.showNumber2 && key != this.number && key != this.oyasumi){
         this.number2 = key;
         this.showNumber2 = true;
-      } else if (key === 'o') {
+      } else if (key === 'o' && this.showNumber) {
         document.body.classList.add('red');
         firstRespondent.classList.add('white');
         secondRespondent.classList.add('white');
@@ -32,8 +33,9 @@ new Vue({
           this.showNumber2 = false;
           this.number = '';
           this.number2 = '';
+          this.oyasumi = '';
         }, 1000);
-      } else if (key === 'x') {
+      } else if (key === 'x' && this.showNumber) {
         document.body.classList.add('blue');
         firstRespondent.classList.add('white');
         secondRespondent.classList.add('white');
@@ -42,6 +44,7 @@ new Vue({
           document.body.classList.remove('blue');
           firstRespondent.classList.remove('white');
           secondRespondent.classList.remove('white');
+          this.oyasumi = this.number;
           if(this.showNumber2){
             playBuzzer();
             this.number = this.number2;
