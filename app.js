a4674371923da35ba7334cacfce096423a818295
Harada <1a88.19e7@gmail.com>
2023-08-09 10:27:21 +0900

    updates

--- app.js
+++ app.js
@@ -13,22 +13,25 @@ new Vue({
       const keyCode = event.keyCode || event.which;
       const firstRespondent = document.getElementsByClassName("first-respondent")[0];
       const secondRespondent = document.getElementsByClassName("second-respondent")[0];
-      if (keyCode >= 48 && keyCode <= 57 && !this.showNumber && key != this.oyasumi) {
+      const oyasumiRespondent = document.getElementsByClassName("oyasumi-respondent")[0];
+      if (((48 <= keyCode && keyCode <= 57) || (96 <= keyCode && keyCode <= 105)) && !this.showNumber && key != this.oyasumi) {
         this.number = key;
         this.showNumber = true;
         playBuzzer();
-      } else if (keyCode >= 48 && keyCode <= 57 && this.showNumber && !this.showNumber2 && key != this.number && key != this.oyasumi){
+      } else if (((48 <= keyCode && keyCode <= 57) || (96 <= keyCode && keyCode <= 105)) && this.showNumber && !this.showNumber2 && key != this.number && key != this.oyasumi){
         this.number2 = key;
         this.showNumber2 = true;
       } else if (key === 'o' && this.showNumber) {
         document.body.classList.add('red');
         firstRespondent.classList.add('white');
         secondRespondent.classList.add('white');
+        oyasumiRespondent.classList.add('white');
         playCorrect();
         setTimeout(() => {
           document.body.classList.remove('red');
           firstRespondent.classList.remove('white');
           secondRespondent.classList.remove('white');
+          oyasumiRespondent.classList.remove('white');
           this.showNumber = false;
           this.showNumber2 = false;
           this.number = '';
@@ -39,11 +42,13 @@ new Vue({
         document.body.classList.add('blue');
         firstRespondent.classList.add('white');
         secondRespondent.classList.add('white');
+        oyasumiRespondent.classList.add('white');
         playWrong();
         setTimeout(() => {
           document.body.classList.remove('blue');
           firstRespondent.classList.remove('white');
           secondRespondent.classList.remove('white');
+          oyasumiRespondent.classList.remove('white');
           this.oyasumi = this.number;
           if(this.showNumber2){
             playBuzzer();
